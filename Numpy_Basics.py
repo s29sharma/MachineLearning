d8ffda54419029d9eb9ab728b6569dacf02b7883
yashicadahiya <s29sharma@gmail.com>
2018-10-19 11:29:37 -0400

    added image-recog

--- Numpy_Basics.py
+++ Numpy_Basics.py
@@ -118,18 +118,30 @@ def image2vector(image):
     return v
 
 
-# This is a 3 by 3 by 2 array, typically images will be (num_px_x, num_px_y,3) where 3 represents the RGB values
-image = np.array([[[ 0.67826139,  0.29380381],
+# This is a 3 by 3 by 2 array, typically images will be (num_px_x, num_px_y,3) where
+# 3 represents the RGB values
+image = np.array(
+    [
+
+        [
+        [ 0.67826139,  0.29380381],
         [ 0.90714982,  0.52835647],
-        [ 0.4215251 ,  0.45017551]],
+        [ 0.4215251 ,  0.45017551]
+        ],
 
-       [[ 0.92814219,  0.96677647],
+       [
+        [ 0.92814219,  0.96677647],
         [ 0.85304703,  0.52351845],
-        [ 0.19981397,  0.27417313]],
+        [ 0.19981397,  0.27417313]
+       ],
 
-       [[ 0.60659855,  0.00533165],
+       [
+        [ 0.60659855,  0.00533165],
         [ 0.10820313,  0.49978937],
-        [ 0.34144279,  0.94630077]]])
+        [ 0.34144279,  0.94630077]
+       ]
+]
+)
 
 print ("image2vector(image) = " + str(image2vector(image)))
 
